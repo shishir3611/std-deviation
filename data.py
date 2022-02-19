eec93e0211c63e2b1342a2f364cb0932ea8b199c
Sheema Parwaz <SheemaParwaz@gmail.com>
2022-02-19 11:17:52 +0530

    debugged

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
-from cmath import sqrt
+
 import csv
 import math
-from pprint import pprint
+
 
 with open('data.csv',newline='') as f:
     reader = csv.reader(f)
@@ -10,23 +10,23 @@ data = file_data[0]
 
 n = len(data)
 
-def mean(data,n=n):
+def mean(data):
     total = 0
     for i in data:
-        total+=float(i)
+        total+=int(i)
     mean = total/n
     return mean
 
 sq_list = []
 for x in data:
-    a = float(x)-mean(data)
+    a = int(x)-mean(data)
     b = a**2
     sq_list.append(b)
 sum = 0
 for y in sq_list:
-    sum+=float(y)
+    sum+=y
 
 divided = sum/(n-1)
-std_deviation = sqrt(divided)
+std_deviation = math.sqrt(divided)
 
 print(std_deviation)
